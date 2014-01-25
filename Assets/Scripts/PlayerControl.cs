e43cf1e3139e208b7cf622369cde68c20095762f
Bryant Cannon <cannb010@guest-wireless-upc-nat-206-117-88-096.usc.edu>
2014-01-24 22:22:49 -0800

    Initial game commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+	public float maxSpeed = 3f;
+	public float forwardAccel = 10f;
+	public float backwardAccel = 20f;
+
+	private float _speed = 0f;
+
+
+	void Update()
+	{
+		float forwardAmt = 0f;
+		bool jump = false;
+
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			forwardAmt -= 1f;
+		}
+		if(Input.GetKey (KeyCode.RightArrow))
+		{
+			forwardAmt += 1f;
+		}
+		if(Input.GetKeyDown (KeyCode.UpArrow))
+		{
+			jump = true;
+		}
+
+
+		if(Mathf.Sign(_speed)==Mathf.Sign(forwardAmt))
+		{
+			_speed = Mathf.MoveTowards(_speed,forwardAmt*maxSpeed,Time.deltaTime * forwardAccel);
+		}
+		else
+		{
+			_speed = Mathf.MoveTowards(_speed,forwardAmt*maxSpeed,Time.deltaTime * backwardAccel);
+		}
+
+
+		Vector2 movement = new Vector2(_speed,rigidbody2D.velocity.y);
+
+		if(jump)
+		{
+			movement.y = 7f;
+		}
+
+		rigidbody2D.velocity = movement;
+	}
+
+}
---
+++ Assets/Scripts/ShiftObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShiftObject : MonoBehaviour
+{
+
+
+	public virtual void OnShift(Perspective perspective)
+	{
+	}
+
+
+
+}
