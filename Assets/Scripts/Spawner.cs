ea676091ba7c5598a9ab94506f551609d60cdc92
Bryant Cannon <cannb010@guest-wireless-upc-nat-206-117-88-096.usc.edu>
2014-01-26 14:42:54 -0800

    Added spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject prefab;
+	public float interval = 3f;
+
+	IEnumerator Start ()
+	{
+		Instantiate (prefab,transform.position,transform.rotation);
+		yield return new WaitForSeconds(interval);
+	}
+
+}
